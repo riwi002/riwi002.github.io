a9cb7ff07aded3fcb5abb875775fbc3df30e365c
riwi002 <riwi001@pm.me>
2023-12-28 03:52:28 +0800

    Site updated: 2023-12-28 03:52:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/17/hello-world/","2023/12/25/参考书/","2023/12/24/ceshi/"];function toRandomPost(){
+var posts=["2023/12/25/Tag Plugins/","2023/12/17/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
