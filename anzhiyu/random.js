fcba98d0a39029cda66c50fe06a6289a2b1516d7
riwi002 <riwi001@pm.me>
2024-01-13 03:39:37 +0800

    Site updated: 2024-01-13 03:39:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/25/Tag Plugins/","2024/01/11/UFW防火墙/","2023/12/17/hello-world/"];function toRandomPost(){
+var posts=["2024/01/11/UFW防火墙/","2023/12/25/Tag Plugins/","2023/12/17/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
